df13a22f638d1d2c05fe3029c9fde75a037c28fc
Nishanthan M Ravichandran <nishanthravi226@gmail.com>
2024-04-25 14:18:01 -0400

    template for register API created

---
+++ CODEBASE/ums/src/main/java/com/n22/infinitude/ums/ahelper/AccountHelper.java
@@ -0,0 +1,13 @@
+package com.n22.infinitude.ums.ahelper;
+
+import java.util.HashMap;
+
+import org.springframework.stereotype.Component;
+@Component
+public class AccountHelper {
+	
+	public String generateEmailAddress(HashMap<String, String> emailContents) {
+		return "success";
+	}
+
+}
--- CODEBASE/ums/src/main/java/com/n22/infinitude/ums/controller/AccountController.java
+++ CODEBASE/ums/src/main/java/com/n22/infinitude/ums/controller/AccountController.java
@@ -1,17 +1,27 @@
 package com.n22.infinitude.ums.controller;
 
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.JsonObject;
+import com.n22.infinitude.ums.ahelper.AccountHelper;
+import com.n22.infinitude.ums.constants.AppConstants;
 
 @RestController 
 @RequestMapping("uwindsor")
 public class AccountController {
+	
+	@Autowired
+	AccountHelper accountHelper;
 	
 	@GetMapping("check")
 	public HttpStatus checkConnection() {
@@ -23,6 +33,36 @@ public class AccountController {
 	public HttpStatus userLogin(@RequestBody JsonObject body) {
 		
 		return HttpStatus.ACCEPTED;
+	}
+	
+	/*Sample request body
+   *{
+    "firstName": "Nishanthan",
+    "middleName": "NA",
+    "lastName": "Modachur Ravichandran",
+    "dob": "2002-06-22",
+    "country": "INDIA",
+    "phoneNumber": "2269611701",
+    "program": "MAC",
+    "degree": "Graduate"
+	 }*/
+	@PostMapping("register/student")
+	public String registerStudent(@RequestParam(name = "params", required = false) HashMap<String, String> params,
+			@RequestHeader(required = false) HashMap<String, String> headers, @RequestBody HashMap<String, String> body) {
+
+		HashMap<String, String> emailContents = new HashMap<>();
+		if (!body.get(AppConstants.MIDDLE_NAME).isBlank()
+				&& !body.get(AppConstants.MIDDLE_NAME).equalsIgnoreCase(AppConstants.NA)) {
+			emailContents.put(AppConstants.EMAIL_ADDRESS, body.get(AppConstants.MIDDLE_NAME));
+		} else if (!body.get(AppConstants.LAST_NAME).isBlank()) {
+			emailContents.put(AppConstants.EMAIL_ADDRESS, body.get(AppConstants.LAST_NAME));
+		}
+
+		emailContents.put(AppConstants.DOB, body.get(AppConstants.DOB));
+
+		String test = accountHelper.generateEmailAddress(emailContents);
+
+		return test;
 	}
 	
 
