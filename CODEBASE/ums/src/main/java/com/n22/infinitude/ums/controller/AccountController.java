af612e88854208bcec30023d4e71b072ccf09a54
Nishanthan M Ravichandran <nishanthravi226@gmail.com>
2024-04-25 12:29:19 -0400

    check configs

--- CODEBASE/ums/src/main/java/com/n22/infinitude/ums/controller/AccountController.java
+++ CODEBASE/ums/src/main/java/com/n22/infinitude/ums/controller/AccountController.java
@@ -2,8 +2,12 @@ package com.n22.infinitude.ums.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.JsonObject;
 
 @RestController 
 @RequestMapping("uwindsor")
@@ -11,6 +15,12 @@ public class AccountController {
 	
 	@GetMapping("check")
 	public HttpStatus checkConnection() {
+		
+		return HttpStatus.ACCEPTED;
+	}
+	
+	@PostMapping("login")
+	public HttpStatus userLogin(@RequestBody JsonObject body) {
 		
 		return HttpStatus.ACCEPTED;
 	}
